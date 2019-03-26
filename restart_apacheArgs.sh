82680928aac17f3e802efaa9d645f56a03db0c27
Mohammad B Meftahi <mmeftahi@hotmail.com>
2019-03-26 16:48:54 -0500

    areguments validation

--- restart_apacheArgs.sh
+++ restart_apacheArgs.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 
+Test=YeS
+
 Config="$1"
 Command="$2"
-
 
 if [ $# -ne 2 ]
 then
     echo "$0 requires at lease two parametere  {virtual-host} {restart|reload}"
     exit 1
+    else
+    printf "\t Required number of arguments are passed = $# \n "
 fi
+
+VHOST_PATH=/etc/apache2/sites-available/
+VHOST_Files=/etc/apache2/sites-available/*.conf
+# Check if config file passed as parameter one exist.....
+#if [ -e /etc/apache2/sites-available/"$Config" ]; then
+if [ -e "$VHOST_PATH$Config" ]; then
+     printf "\t Config File exists. \n"
+else
+    printf "\t Provide File does not exist \n"
+    printf "\t Config Files are below: \n "
+    for fileName in $VHOST_Files
+    do
+        printf "\t $fileName \n"
+        fileOnly=$(basename $fileName)
+        printf "\t\t $fileOnly \n"
+        [ "$Test" == "Yes" ] && ( printf "\t echo for testing  $fileName \n")
+    done
+    exit 1
+fi
+
+echo " passed the file check"
+exit 0
+
 
 if [ "$Command" == "restart" ] || [ "$Command" == "reload" ]
 then
